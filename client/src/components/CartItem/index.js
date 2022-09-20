57c9a34372283115c7ac0e8807221ea15db2beb7
willig0203 <100536076+willig0203@users.noreply.github.com>
2022-09-20 17:06:14 -0500

    shopping cart fully functional

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,6 +1,35 @@
 import React from "react";
 
+import { useStoreContext } from "../../utils/GlobalState";
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from "../../utils/actions";
+
 const CartItem = ({ item }) => {
+  const [, dispatch] = useStoreContext();
+
+  const removeFromCart = (item) => {
+    dispatch({
+      type: REMOVE_FROM_CART,
+      _id: item._id,
+    });
+  };
+
+  const onChange = (e) => {
+    const value = e.target.value;
+
+    if (value === "0") {
+      dispatch({
+        type: REMOVE_FROM_CART,
+        _id: item._id,
+      });
+    } else {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: item._id,
+        purchaseQuantity: parseInt(value),
+      });
+    }
+  };
+
   return (
     <div className="flex-row">
       <div>
@@ -12,8 +41,17 @@ const CartItem = ({ item }) => {
         </div>
         <div>
           <span>Qty:</span>
-          <input type="number" placeholder="1" value={item.purchaseQuantity} />
-          <span role="img" aria-label="trash">
+          <input
+            type="number"
+            placeholder="1"
+            value={item.purchaseQuantity}
+            onChange={onChange}
+          />{" "}
+          <span
+            role="img"
+            aria-label="trash"
+            onClick={() => removeFromCart(item)}
+          >
             🗑️
           </span>
         </div>
